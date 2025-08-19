27fd7b4df28c2a24fe3ab971ff2c8e1ad8834abc
Jesus Chapi <jesersu>
2025-08-19 13:15:15 -0500

    first commit, start project

---
+++ CountryApp/CountryApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountryApp
+//
+//  Created by Jesús Ervin Chapi Suyo on 19/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountryApp/CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by Jesús Ervin Chapi Suyo on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
