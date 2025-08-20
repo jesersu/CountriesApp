1b5be25726eb16bbc46bdf272a63ce77a08284c5
Jesus Chapi <jesersu@gmail.com>
2025-08-20 04:34:23 -0500

    Add error handler in CountryService

--- CountryApp/CountryApp/Data/CountryService.swift
+++ CountryApp/CountryApp/Data/CountryService.swift
@@ -13,7 +13,49 @@ class CountryService {
     
     func fetchAllCountries() async throws -> [Country] {
         print("url : \(url)")
-        let (data, _) = try await URLSession.shared.data(from: url)
+        let (data, response) = try await URLSession.shared.data(from: url)
+       
+        if let httResponse = response as? HTTPURLResponse {
+            let statusCode = httResponse.statusCode
+            
+            switch statusCode {
+                case 200...299:
+                    break;
+            default:
+                let errorFinded = parseError(data: data, statusCode: statusCode)
+                throw errorFinded
+            }
+        }
+        
         return try JSONDecoder().decode([Country].self, from: data)
     }
-} 
+    
+    private func parseError(data: Data, statusCode: Int) -> CountryServiceError {
+        if statusCode == 401 {
+            return .notToken
+        } else if statusCode == 403 {
+            return CountryServiceError.unauthorized
+        } else {
+            return CountryServiceError.badRequest
+        }
+    }
+}
+
+enum CountryServiceError: String, Error {
+    case notToken = "401 not token"
+    case unauthorized = "403 sin opermiso"
+    case badRequest = "404 bad request"
+}
+
+extension CountryServiceError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .notToken:
+            return "bad Token"
+        case .unauthorized:
+            return "Without Permission"
+        case .badRequest:
+            return "Bad Request"
+        }
+    }
+}
---
+++ CountryApp/CountryApp/Helpers/Endpoint/SettingEndpoint.swift
@@ -0,0 +1,36 @@
+//
+//  SettingEndpoint.swift
+//  CountryApp
+//
+//  Created by Jesús Ervin Chapi Suyo on 20/08/25.
+//
+import Persist
+struct SettingEndpoint {
+    enum UrlType: String {
+        case preProduction, production
+
+        var urlString: String {
+            switch self {
+            case .preProduction:
+                return "https://restcountries.com/v3"
+            case .production:
+                return "https://restcountries.com/v3.1"
+            }
+        }
+    }
+
+    static func getUrl() -> UrlType {
+        let urlSaved = Persist.getCacheData(key: KeysCache.environmentType)
+        //let urlSaved = KeychainManager.retrieveAttribute(key: KeysCache.environmentType)
+
+        switch urlSaved {
+        case UrlType.preProduction.rawValue:
+            return UrlType.preProduction
+        case UrlType.production.rawValue:
+            return UrlType.production
+        default:
+            return UrlType.production
+        }
+    }
+    
+}
